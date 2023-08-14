519d262c6e3d06f8b7169143679ee3a4124f93ef
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-08-14 17:20:04 +0200

    refactoring

---
+++ php/templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./scss/compiled_css/main.css">
+    <title>Hotel Miranda</title>
+</head>
