2e99e175687c82fc1ffef705f34891c7d84e1433
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-08-14 16:09:33 +0200

    sass configuration ok

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./scss/compiled_css/main.css">
     <title>Hotel Miranda</title>
 </head>
 <body>
 
+<div>
+    
+</div>
     <h1>Hotel Mirandas</h1>
     
 </body>
