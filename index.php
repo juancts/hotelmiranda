9bf2ee146a645af6db7d92c75989738d5e6b2e90
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-08-14 18:27:47 +0200

    icons node

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-    include 'php/templates/header.php'
+    include 'dist/php/templates/header.php'
 ?>
 <body>
 
 <div>
     
 </div>
-    <h1>Hotel Mirandas</h1>
+   
     
 </body>
 </html>
