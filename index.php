76a6c27d496a7613ddbed3b571e35822d03209ce
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-08-14 23:13:36 +0200

    mobile header

--- index.php
+++ index.php
@@ -3,10 +3,25 @@
 ?>
 <body>
 
-<div>
-    
-</div>
-   
+
+//BANNER TOP
+<section class="banner">
+
+</section>
+
+
+//SECTION DATES OF ARRIBAL DEPARTURE
+<section class="dates">
+
+</section>
+
+//SECTION ABOUT Us
+<section class="about">
+</section>
+
+//SECTION 
+
+
     
 </body>
 </html>
