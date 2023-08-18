18bb60735f48d9ce62589b92baa91cb736c2374f
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-08-18 15:36:30 +0200

    borro dist de .gitignorre para deploy

---
+++ dist/php/templates/footer.php
@@ -0,0 +1,108 @@
+<footer class="footer">
+  <div class="footer__content">
+    <div class="footer__logo">
+      <div class="footer__logo--figure">
+        <span class="footer__logo--letter">H</span>
+      </div>
+
+      <div class="navbar__title">
+        <p class="navbar__title--strong">HOTEL</p>
+        <p class="navbar__title--thin">MIRANDA</p>
+      </div>
+    </div>
+    <div class="footer__uppertext">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, amet
+        corporis temporibus, quis repudiandae necessitatibus molestiae voluptate
+        cupiditate nostrum at ipsa minus ipsam animi ratione eaque veniam nobis
+        deserunt vitae.
+      </p>
+    </div>
+    <div class="footer__redes">
+      <a href="#" class="footer__redes--link"
+        ><i class="fab fa-facebook"></i
+      ></a>
+
+      <a href="#" class="footer__redes--link footer__redes--twitter">
+        <i class="fab fa-twitter"></i>
+      </a>
+
+      <a href="#" class="footer__redes--link"><i class="fab fa-behance"></i></a>
+      <a href="#" class="footer__redes--link"
+        ><i class="fab fa-linkedin"></i
+      ></a>
+      <a href="#" class="footer__redes--link"><i class="fab fa-youtube"></i></a>
+    </div>
+
+    <!-- SERVICIOS -->
+    <div class="footer__services">
+      <h1 class="footer__services--title">Services.</h1>
+    </div>
+    <div class="footer__services--list">
+      <div>
+        <ul>
+          <li>Resturent & Bar</li>
+          <li>Swimming Pool</li>
+          <li>Wellness & Spa</li>
+          <li>Restaurant</li>
+          <li>Conference Room</li>
+          <li>Coctail Party House</li>
+        </ul>
+      </div>
+      <div id="list2">
+        <ul>
+          <li>Gaming Zone</li>
+          <li>Marriage Party</li>
+          <li>Party Planning</li>
+          <li>Tour Consultancy</li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- CONTACT US -->
+    <div class="footer__contactus">
+      <div class="footer__contactus--title">
+        <h1>Contact Us.</h1>
+      </div>
+      <div class="footer__contactus--card">
+        <div class="footer__contactus--icon">
+          <i class="fas fa-phone"></i>
+        </div>
+        <div class="footer__contactus--description">
+          <h1>Phone Number</h1>
+          <h3>+987 876 765 76 577</h3>
+        </div>
+      </div>
+      <!-- EMAIL -->
+      <div class="footer__contactus--card">
+        <div class="footer__contactus--icon">
+          <i class="fas fa-envelope"></i>
+        </div>
+        <div class="footer__contactus--description">
+          <h1>Email</h1>
+          <h3>jotarodriguez@gmail.com</h3>
+        </div>
+      </div>
+
+      <!-- UBICACION -->
+      <div class="footer__contactus--card">
+        <div class="footer__contactus--icon">
+          <i class="fas fa-map-marker-alt"></i>
+        </div>
+        <div class="footer__contactus--description">
+          <h1>Ubicacion</h1>
+          <h3>Lorem ipsum dolor sit</h3>
+        </div>
+      </div>
+    </div>
+   
+  </div>
+   <!-- COPIRIGHT -->
+   <div class="footer__copyright">
+    <div class="footer__copyright--content">
+        <h3 id="margin">Copyright By@Example - 2023</h3>
+        <h3>Terms of use | Privacy Enviromental Policy</h3>
+
+    </div>
+</div>
+</footer>
---
+++ dist/php/templates/navbar.php
@@ -0,0 +1,42 @@
+
+<section class="navbar">
+    <div class="navbar__content">
+        <div class="navbar__toggle">
+            <i class="fas fa-bars"></i>
+        </div>
+        <div class="navbar__square">
+            <div class="navbar__square--figure">
+                <span class="navbar__letter">H</span>
+            </div>
+    
+        </div>
+        <div class="navbar__title">
+            <p class="navbar__title--strong">HOTEL</p>
+            <p class="navbar__title--thin">MIRANDA</p>
+        </div>
+        <div class="navbar__usersearch">
+            <div class="navbar__toggle navbar__toggle--user">
+                <i class="fas fa-user"></i>
+            </div>
+            <div class="navbar__toggle navbar__toggle--search">
+                <i class="fas fa-search"></i>
+            </div>
+        </div>
+           
+        
+        <nav class="navbar__nav">
+            
+            <ul class="navbar__menu">
+                <li class="navbar-item"><a href="#" class="navbar-link">About Us</a></li>
+                <li class="navbar-item"><a href="#" class="navbar-link">Rooms</a></li>
+                <li class="navbar-item"><a href="#" class="navbar-link">Offers</a></li>
+                <li class="navbar-item"><a href="#" class="navbar-link">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+    
+</section>
+
+
+   
+
