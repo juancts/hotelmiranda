159e8534c1a17aa0b027b9b43cebc76cd96adb2c
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-08-18 15:59:41 +0200

    changed name main to style.scss

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         href="https://fonts.googleapis.com/css2?family=Archivo:wght@100;500;600&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&display=swap"
         rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="./scss/main.css" />
+    <link rel="stylesheet" href="./scss/style.css" />
     <title>Hotel Miranda</title>
 </head>
 
