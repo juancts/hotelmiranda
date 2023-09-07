7324aff78fa95296f7c1e3b3224079153ce27983
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-09-07 23:39:48 +0200

    js

---
+++ js/navbar.js
@@ -0,0 +1,11 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerButton = document.getElementById("hambur");
+  
+    hamburgerButton.addEventListener("click", function () {
+      console.log("Hola");
+    });
+  });
+  
