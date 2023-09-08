2e4d40f436e2d9f7d418d7ee3368f68f92c5304f
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-09-08 16:45:05 +0200

    navbar mobile

--- js/navbar.js
+++ js/navbar.js
@@ -3,9 +3,9 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     const hamburgerButton = document.getElementById("hambur");
-  
+    const navbarNav = document.getElementById("navbarNav");
     hamburgerButton.addEventListener("click", function () {
-      console.log("Hola");
+      navbarNav.style.display = (navbarNav.style.display === 'none' || navbarNav.style.display === '') ? 'block' : 'none';
     });
   });
   
